368a67ae4287bd9355a602910ca69ea24c75592a
Joey Kalbas <jkalbas@email.unc.edu>
2022-02-04 13:18:51 -0500

    added exit statement if no "call" flag is inputted

--- guess-flip.js
+++ guess-flip.js
@@ -3,5 +3,7 @@ var args = process.argv.slice(2);
 const guess = args[0].replace('--call=', '')
 
 if (guess != 'heads' && guess != 'tails') {
-    
+    console.log('Error: incorrect input. \nUsage: "node guess-flip --call=[heads|tails]')
+    process.exit(1)
 }
+
