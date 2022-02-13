f3c04657755940a1513018cba12b2b9e73bed4a2
Joey Kalbas <jkalbas@email.unc.edu>
2022-02-12 23:57:08 -0600

    changed how guess is defined

--- guess-flip.js
+++ guess-flip.js
@@ -1,10 +1,12 @@
 var args = process.argv.slice(2);
 
+var guess = ''
+
 if(args[0] == null) {
     console.log('Error: no input.', '\n','Usage: node guess-flip --call=[heads|tails]')
+} else {
+    guess = args[0].replace('--call=', '')
 }
-
-const guess = args[0].replace('--call=', '')
 
 if (guess != 'heads' && guess != 'tails') {
     console.log('Error: incorrect input. \nUsage: node guess-flip --call=[heads|tails]')
