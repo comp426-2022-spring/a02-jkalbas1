bb7739eb775920469a850ae647eccf84069fe061
Joey Kalbas <jkalbas@email.unc.edu>
2022-02-12 23:14:24 -0600

    set numflips = 1 if no args for flips

--- flips.js
+++ flips.js
@@ -1,12 +1,10 @@
 var args = process.argv.slice(2);
 
-if(args[0] == null) {
-    console.log('Error: no input. \nUsage: "node guess-flip --call=[heads|tails]')
-    process.exit(1)
+if( args[0] != null) {
+    const number = args[0].replace('--number=', '')
+} else {
+    const number = 1
 }
-
-const number = args[0].replace('--number=', '') || 1
-
 import { coinFlips, countFlips } from './modules/coin.mjs'
 
 let flips = coinFlips(number)
