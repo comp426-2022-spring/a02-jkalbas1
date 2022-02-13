25cab985708712b3791d400c6fb1e98098ed3d85
Joey Kalbas <jkalbas@email.unc.edu>
2022-02-12 23:38:52 -0600

    added newline after flips

--- flips.js
+++ flips.js
@@ -1,7 +1,7 @@
 var args = process.argv.slice(2)
 
 var number = 0
-if (args[0] == null || args[0] == '--number=') {
+if (args[0] == null) {
     number = 1
 } else {
     number = args[0].replace('--number=', '')
@@ -14,5 +14,5 @@ if (number != 1) {
     let flip_data = countFlips(flips)
     console.log(flips, '\n', flip_data)
 } else {
-    console.log(flips)
+    console.log(flips, '\n')
 }
